c42a0bf6e4bec070e38b7f45e1f04b73779c8667
narasimhareddyp23 <61200507+narasimhareddyp23@users.noreply.github.com>
2020-03-26 15:09:54 +0530

    2403

---
+++ var_addr.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+
+
+
+//int a
+//char ch
+
+//int a[2]
+
+
+
+int main()
+{
+
+	int var1 = 20;
+	char ch1 = 21;
+	int arr1[2]= {1,2};
+	
+
+/*
+1)    int a;
+a = 20;
+
+(or)
+
+2)int a =20;
+
+
+pointer:
+
+1) int *ptr;
+
+ptr = &var1;
+
+(or)
+
+2) int *ptr = &var1;
+
+
+
+
+*/
+	int* straddre;// pointer decalration -> datatype* varName;
+	printf("address of var1:%p\n", &var1);
+//	printf("address of ch1:%p\n", &ch1);
+		
+//	printf("address a[0]:%p\n, address a[1]:%p\n", &arr1[0],&arr1[1]);
+	
+	straddre = &var1;// assigning address
+	printf("value of straddre:%p\n", straddre);	
+	printf("value of var1:%d\n", var1);	
+
+
+
+//	int *ptr = &var1;//decalartion and initialization
+	
+	*straddre = 30; //dereference
+	
+	printf("value of var1:%d\n", var1);	
+
+	
+}
